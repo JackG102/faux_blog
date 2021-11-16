e2e79be3de465344e4b6bd6c2f419b328677b29e
Jack Garratt <jack.garratt@icfnext.com>
2021-11-16 06:15:10 -0500

    Created header

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div class="ui three item menu">
+      <a class="item active">Blog</a>
+      <a class="item">Add Blog</a>
+      <a class="item">About</a>
+    </div>
+  );
+}
+
+export default Header;
