8c43674d2e42047082dd3f407dd48e2687391c6e
Jack Garratt <jack.garratt@icfnext.com>
2021-11-18 20:08:17 -0500

    Added rudimentary routing for real

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import Link from './Link';
 const Header = () => {
   return (
     <div className="ui three item menu">
-      <a href="/" className="item active">Blog</a>
-      <a href="/add_blog" className="item">Add Blog</a>
-      <a href="/" className="item">About</a>
+      <Link href="/" className="item active">Accordion</Link>
+      <Link href="/add_blog" className="item">Add Blog</Link>
+      <Link href="/about" className="item">About</Link>
     </div>
   );
 }
